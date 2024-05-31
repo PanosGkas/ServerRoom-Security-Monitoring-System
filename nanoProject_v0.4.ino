6077c2b782560735865850a76c3eafc221625a00
PanosGkas <117219816+PanosGkas@users.noreply.github.com>
2024-05-31 12:58:01 +0300

    Adding code for NFC and Servo Motor. Improved DHT function

--- nanoProject_v0.4.ino
+++ nanoProject_v0.4.ino
@@ -1,18 +1,28 @@
-#include <Servo.h>
-#include <DHT.h> 
+#include <Servo.h> // Servo Library
+#include <DHT.h> // Humidity Temprature Library
+
+
+#include <SPI.h>
+#include <MFRC522.h> // RFID sensor Library
 
 //define the sensor pins
 #define smokeSensorPin A0
+#define motionSensorPin 2
+#define servoPin 3
 #define buzzerPin 4
 #define redLedPin 5
 #define blueLedPin 6
 #define greenLedPin 7
 #define tempSensorPin 8 
-#define motionSensorPin 9
-#define servoPin 10
+
 #define DHTTYPE DHT11
 
-//creation of objects
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+//object creation and initial values definition
 Servo servo;
 DHT tempSensor(tempSensorPin, DHTTYPE);   
 int motionSensorInput = LOW;
@@ -21,15 +31,22 @@ int smokeSensorValue;
 void setup() {
   //initialize pin headers
   Serial.begin(9600);
-  servo.attach(10);
+  servo.attach(servoPin);
   servo.write(0);
   tempSensor.begin();
   pinMode(redLedPin, OUTPUT);
-  pinMode(blueLedPin, OUTPUT);
+  pinMode(blueLedPin, OUTPUT); 
   pinMode(greenLedPin, OUTPUT);
   pinMode(motionSensorInput, INPUT); 
   pinMode(buzzerPin, OUTPUT);
   pinMode(smokeSensorPin, INPUT);
+
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  delay(4);
+  mfrc522.PCD_DumpVersionToSerial();
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
   }
 
   void loop(){
@@ -37,14 +54,63 @@ void setup() {
     //soundBuzzer();
     //servoMovement();
     //checkSmoke();
-    checkTemp();
-    //servoMovement();
+    //checkTemp();
     //testLED();
+    checkRFID();
   }
+
+//function for RFID
+void checkRFID(){
+
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "23 5A F8 33") //εδώ πρέπει να αλλάξουμε το UID που θέλουμε εμείς
+  {
+    
+    Serial.println("Authorized access");
+    analogWrite(greenLedPin, 255); // με το delay το χρώμα θα κρατήσει για 3 sec
+    Serial.println();
+    soundBuzzer();
+    stopBuzzer();
+    openDoor();
+    analogWrite(greenLedPin, 0);
+
+  }
+ 
+ else   {
+    Serial.println("Access denied");
+    analogWrite(redLedPin, 255);
+    delay(3000);
+    analogWrite(redLedPin, 0);
+  }
+    mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+} 
 
 
 //function to test cathode LEDs
-
 void testLED(){
 analogWrite(blueLedPin, 256);
 delay(2000);
@@ -65,6 +131,7 @@ void openDoor(){
   servo.write(90);
   delay(5000);
   servo.write(0);
+  delay(1000);
 }
 
 //function for motion detection with buzzer alarm, blue LED indicator and serial print
@@ -111,7 +178,7 @@ void stopBuzzer(){
 
 void soundBuzzer(){
   digitalWrite(buzzerPin, HIGH);
-  delay(1000); 
+  delay(500); 
 }
 
 //!!!NOT TESTED!!!
